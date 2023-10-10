cdc6e8798d85cdef60db2442dc5bf75127ceb120
Hollen <yujianni666@icloud.com>
2023-10-10 13:01:01 -0400

    project setup

---
+++ BlueDevilBites/BlueDevilBites/BlueDevilBitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlueDevilBitesApp.swift
+//  BlueDevilBites
+//
+//  Created by Hollen Zhang on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct BlueDevilBitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
