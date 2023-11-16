c2fbc2236c9ef1c07fc18b703d6cb580da74ca3c
Hollen <yujianni666@icloud.com>
2023-11-15 22:24:26 -0500

    Show N/A when the phone number is not available

--- BlueDevilBites/BlueDevilBites/View/Restaurants/RestaurantDetailView.swift
+++ BlueDevilBites/BlueDevilBites/View/Restaurants/RestaurantDetailView.swift
@@ -12,40 +12,50 @@ struct RestaurantDetailView: View {
     var restaurant: Res
 
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading, spacing: 10) {
-                Text(restaurant.name)
-                    .font(.largeTitle)
-                    .bold()
-
-                if let phone = restaurant.phone {
+        GeometryReader { geometry in
+            ScrollView {
+                VStack(alignment: .leading, spacing: 10) {
+                    
+                    Image("\(restaurant.name)_1") // Use the restaurant's name with the index
+                        .resizable()
+                        .frame(width: geometry.size.width, height: geometry.size.height / 3)
+                    
+                    Text(restaurant.name)
+                        .font(.largeTitle)
+                        .bold()
+                    
+                    if let phone = restaurant.phone {
+                        // Updated code for handling phone number
+                        HStack {
+                            Image(systemName: "phone.fill")
+                            // Check for both nil and empty string
+                            Text((restaurant.phone?.isEmpty ?? true) ? "N/A" : restaurant.phone!)
+                        }
+                    }
+                    
+                    Text("Location: \(restaurant.location)")
+                    Text("Status: \(restaurant.isOpen ? "Yes" : "No")")
+                    
+                    if let googleMapURL = restaurant.position.googleMap,
+                       let url = URL(string: googleMapURL) {
+                        MapView(url: url)
+                            .frame(height: 300)
+                    }
+                    
                     HStack {
-                        Image(systemName: "phone.fill")
-                        Text(phone)
+                        Text("Tags: ")
+                        ForEach(restaurant.tags, id: \.self) { tag in
+                            Text(tag)
+//                                .padding(5)
+                                .background(Color.blue)
+                                .foregroundColor(.white)
+                                .cornerRadius(5)
+                        }
                     }
                 }
-
-                Text("Location: \(restaurant.location)")
-                Text("Open Now: \(restaurant.isOpen ? "Yes" : "No")")
-
-                if let googleMapURL = restaurant.position.googleMap,
-                   let url = URL(string: googleMapURL) {
-                    MapView(url: url)
-                        .frame(height: 300)
-                }
-
-                HStack {
-                    Text("Tags: ")
-                    ForEach(restaurant.tags, id: \.self) { tag in
-                        Text(tag)
-                            .padding(5)
-                            .background(Color.blue)
-                            .foregroundColor(.white)
-                            .cornerRadius(5)
-                    }
-                }
+//                .padding()
             }
-            .padding()
+            .ignoresSafeArea()
         }
     }
 }
@@ -68,7 +78,7 @@ struct RestaurantDetailView_Previews: PreviewProvider {
         RestaurantDetailView(restaurant: Res(
             placeIdString: "1",
             isOpenString: "true",
-            name: "Sample Restaurant",
+            name: "McDonald's",
             position: Res.Position(latitude: "40.7128", longitude: "-74.0060", googleMap: "http://maps.google.com"),
             location: "123 Main St",
             phone: "123-456-7890",
